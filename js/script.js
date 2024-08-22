1e47e23f0b9bb261b98557a80db1b4f2b07dd624
carlaaguilar20 <carlaaguilar20@gmail.com>
2024-08-21 21:52:20 -0300

    metodo filter

--- js/script.js
+++ js/script.js
@@ -1,42 +1,12 @@
-class destino {
-    informacion(ciudad, diasdisponible, mesdisponible, presupuesto) {
-        this.ciudad= ciudad;
-        this.diasdisponible= diasdisponible;
-        this.mesdisponible= mesdisponible;
-        this.presupuesto= presupuesto;
-    }
-}
- const tokio = new destino ("tokio",20,21,22,23,"mayo",3000);
- const paris = new destino ("paris",21,22,23,24,"septiembre",5000);
- const londres= new destino ("londres",17,18,19,20,"abril",6000);
- const venecia= new destino ("venecia",19,20,21,22,"diciembre",8000);
+let destino = [
+    {ciudad: "venecia", disponibilidad: 15},
+    {ciudad: "tokio", disponibilidad: 20},
+    {ciudad: "londres", disponibilidad: 1},
+    {ciudad: "paris", disponibilidad: 9},
+]
+ let informacion = prompt ("ingrese el destino requerido")
+let destinos = destino.filter(Element => Element.ciudad == informacion)
 
- const destinos = [
-    tokio,
-    paris,
-    londres,
-    venecia,
-
- ];
-
- function filtrardestino (destinos,buscar){
-    return destinos.filter(
-        (destino)=>
-            destino.ciudad.toLowerCase().includes(buscar.toLowerCase())||
-            destino.presupuesto.toLowerCase().includes(buscar.toLowerCase())
-
-    )
- }
- const buscar = prompt("ingrese la ciudad que desea visitar:");
- const resultados= filtrardestino(destinos,buscar);
-
- if (resultados.length > 0) {
-    alert("juegos encontrados:");
-    resultados.forEach((juego)=> {
-        alert(
-            "ciudad:${destino.ciudad}- diasdisponible: ${destino.diasdisponible} - mesdisponible: ${destino.mesdisponible} - presupuesto: ${destino.presupuesto}"
-        );
-    });
- } else{
-    alert ("No se encuentra disponible la ciudad ingresada.");
- }
+console.log(destinos);
+alert
+(informacion)
