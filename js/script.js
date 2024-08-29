945a556c860e7eb8334faf1ad4dbfd6a761b49d2
carlaaguilar20 <carlaaguilar20@gmail.com>
2024-08-28 23:35:06 -0300

    json storage evento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-let destino = [
-    {ciudad: "venecia", disponibilidad: 15},
-    {ciudad: "tokio", disponibilidad: 20},
-    {ciudad: "londres", disponibilidad: 1},
-    {ciudad: "paris", disponibilidad: 9},
-]
- let informacion = prompt ("ingrese el destino requerido")
-let destinos = destino.filter(Element => Element.ciudad == informacion)
-
+const destinos = [
+    {
+        lugar: "tokio",
+        diasDisponibles : ( 14,15,16,17),
+        mesDispobible: "mayo",
+        presupuestoEn$: 6000
+    },
+    {
+        lugar: "londres",
+        diasDisponibles : ( 24,25,26,27),
+        mesDispobible: "abril",
+        presupuestoEn$: 20000
+    },
+    {
+        lugar: "paris",
+        diasDisponibles : ( 4,5,6,8),
+        mesDispobible: "diciembre",
+        presupuestoEn$: 22000
+    },
+    {
+        lugar: "venecia",
+        diasDisponibles : ( 10,11,12,13),
+        mesDispobible: "septiembre",
+        presupuestoEn$: 8000
+    }
+] 
+const destinosJSON = JSON.stringify(destinos);
 console.log(destinos);
-alert
-(informacion)
+console.log(destinosJSON);
+ const guardarEnelLocalStorage = (key , value ) => {localStorage.setItem(key,value)};
+ for ( const destino of destinos) {
+    guardarEnelLocalStorage(destino.lugar , JSON.stringify(destino));
+ }
+  
+const imagen = document.querySelector('#torreE');
+imagen.addEventListener("mouseover" , () =>{
+    console.log("mouse sobre imagen")
+    imagen.style.width ="60%";
+})
+
+const imagen2 = document.querySelector('#puenteB');
+imagen2.addEventListener("mouseover" , () =>{
+    console.log("mouse sobre imagen")
+    imagen2.style.width ="70%";
+})
